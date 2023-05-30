ae5cd4344cad4569a84e8b45045a4910428c85be
Mrdeck <mrdeck30@gmail.com>
2023-05-30 16:10:34 +0100

    updat auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,11 +10,13 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    {   
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('non');
+            $table->string('prenom');
             $table->string('email')->unique();
+            $table->string('telephone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- resources/views/components/auth/auth-header.blade.php
+++ resources/views/components/auth/auth-header.blade.php
@@ -3,7 +3,7 @@
   <header id="header" class="header d-flex align-items-center bg-dark">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="/" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
         <h1>BuildMaster<span>.</span></h1>
@@ -13,7 +13,7 @@
       <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a href="index.html" class="active">Accueil</a></li>
+          <li><a href="/" class="active">Accueil</a></li>
           <li><a href="about.html">A propos</a></li>
           <li><a href="services.html">Services</a></li>
           <li><a href="projects.html">Projets</a></li>
